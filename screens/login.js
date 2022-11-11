8bd69d8cb067a10b95f6061d9be6824d1d695c8b
learningnoobi <fanime492@gmail.com>
2022-11-12 03:53:51 +0545

    fix : changed ui for login and register page

--- screens/login.js
+++ screens/login.js
@@ -1,16 +1,24 @@
-import { Text, Input, Spinner,Button } from "@ui-kitten/components";
-// import { Button } from "react-native";
+import { Text, Input, Spinner, Button } from "@ui-kitten/components";
+import { View, StyleSheet,Image } from "react-native";
 import { useState } from "react";
 import SafeAreaView from "../components/SafeAreaView";
 import showToast from "../utils/toast";
 import { axiosInstance } from "../utils/axios";
 import useUserStore from "../store";
+import { height, width } from "../constant/size";
+import LogoIcon from "../assets/logo_a.png";
+
+const LoadingIndicator = (props) => (
+  <View style={[props.style, styles.indicator]}>
+    <Spinner size="small" />
+  </View>
+);
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState("overlord123@gmail.com");
   const [password, setPassword] = useState("overlord123");
   const [loading, setLoading] = useState(false);
-  const setToken = useUserStore((store) => store.setToken)
+  const setToken = useUserStore((store) => store.setToken);
 
   const initLogin = async () => {
     if (!email.trim()) {
@@ -23,12 +31,12 @@ const LoginScreen = ({ navigation }) => {
     }
     setLoading(true);
     try {
-      const {data} = await axiosInstance.post("/users/token/", {
+      const { data } = await axiosInstance.post("/users/token/", {
         email: email,
-        password: password
-      })
-      console.log("SADD", data.access)
-      setToken(data.access)
+        password: password,
+      });
+      console.log("SADD", data.access);
+      setToken(data.access);
     } catch (err) {
       // console.error(JSON.stringify(err))
       showToast("LOGIN ERROR");
@@ -38,40 +46,87 @@ const LoginScreen = ({ navigation }) => {
   };
   return (
     <SafeAreaView>
-      <Text>Login</Text>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <>
-          <Input
-            value={email}
-            placeholder="Email"
-            onChangeText={(nextValue) => setEmail(nextValue)}
-          ></Input>
-          <Input
-            value={password}
-            placeholder="Password"
-            onChangeText={(nextValue) => setPassword(nextValue)}
-          ></Input>
-
-          <Button onPress={initLogin}>Login</Button>
-
-          <Text>
-            Don't have an account?
-            <Text
-              style={{ color: "#222", fontWeight: "bold" }}
-              onPress={() => {
-                navigation.navigate("Register");
-              }}
-            >
-              {" "}
-              Register
-            </Text>
+      <View style={styles.container}>
+        <View style={styles.center}>
+        <Image
+            style={styles.logo}
+            source={LogoIcon}
+          />
+         <View>
+         <Text category="h2" style={{...styles.mg10}}>
+            Welcome Back
           </Text>
-        </>
-      )}
+          <Text category="h5" style={{color:'#333'}}>
+            Login
+          </Text>
+         </View>
+        </View>
+        <View style={{marginTop:height*0.04}}>
+        <Input
+          style={styles.mg10}
+          value={email}
+          placeholder="Email"
+          size="large"
+          onChangeText={(nextValue) => setEmail(nextValue)}
+        ></Input>
+        <Input
+          style={styles.mg10}
+          value={password}
+          keyboardType={'visible-password'}
+          size="large"
+          placeholder="Password"
+          onChangeText={(nextValue) => setPassword(nextValue)}
+        ></Input>
+        </View>
+
+        <Button
+
+          style={{...styles.mg10,backgroundColor:'#02BA76',borderColor:'#02BA76'}}
+          onPress={initLogin}
+          accessoryLeft={loading && LoadingIndicator}
+        >
+          Login {/* {loading ?    <Spinner size='small'/>:'Login'} */}
+        </Button>
+
+        <Text style={{marginTop:20,textAlign:'center',fontWeight:'medium',fontSize:18}}>
+          Don't have an account?
+          <Text
+            style={{ color: "#02BA76", fontWeight: "bold",fontSize:19 }}
+            onPress={() => {
+              navigation.navigate("Register");
+            }}
+          >
+            {" "}
+            Register
+          </Text>
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    height: "100%",
+    display: "flex",
+    paddingHorizontal: width * 0.03,
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  mg10: {
+    marginVertical: 10,
+  },
+  center: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection:'row'
+  },
+  logo: {
+    width: 80,
+    height: 80,
+  },
+});
--- screens/register.js
+++ screens/register.js
@@ -5,16 +5,18 @@ import {
   Select,
   SelectItem,
   IndexPath,
-  Button
+  Button,
 } from "@ui-kitten/components";
-import { AsyncStorage } from "react-native";
 import { useState } from "react";
 import SafeAreaView from "../components/SafeAreaView";
 import showToast from "../utils/toast";
 import ROLES from "../constant/roles";
 import { axiosInstance } from "../utils/axios";
+import { View, StyleSheet, AsyncStorage, Image } from "react-native";
+import { height, width } from "../constant/size";
+import LogoIcon from "../assets/logo_a.png";
 
-const RegisterScreen = ({navigation}) => {
+const RegisterScreen = ({ navigation }) => {
   const [username, setUsername] = useState("overlord123");
   const [email, setEmail] = useState("overlord123@gmail.com");
   const [password1, setPassword1] = useState("overlord123");
@@ -41,71 +43,102 @@ const RegisterScreen = ({navigation}) => {
     }
     setRegistering(true);
     try {
-      const {data} = await axiosInstance.post("users/register/", {
+      const { data } = await axiosInstance.post("users/register/", {
         username: username,
         email: email,
         password: password1,
         password_confirm: password2,
         role: ROLES[selectedIndex.row],
-      })
-      console.log("Register", {data})
+      });
+      console.log("Register", { data });
       showToast("Registered successfully. Please Log in.");
 
-      await AsyncStorage.setItem(`pk_${data.data.username}`, data.private_key)
+      await AsyncStorage.setItem(`pk_${data.data.username}`, data.private_key);
       setTimeout(() => {
         navigation.navigate("Login");
       }, 500);
       // setToken(data.access)
     } catch (err) {
       // console.log(JSON.stringify(err))
-      console.log(err.response?.data?.detail)
+      console.log(err.response?.data?.detail);
       showToast(err.response?.data?.detail);
     } finally {
-        setRegistering(false);
+      setRegistering(false);
     }
   };
   return (
     <SafeAreaView>
-      <Text>Register</Text>
-      {registering ? (
-        <Spinner/>
-      ) : (
-        <>
-          <Input
-            value={username}
-            label={"Username"}
-            placeholder="Username"
-            type
-            onChangeText={(nextValue) => setUsername(nextValue)}
-          ></Input>
-          <Input
-            value={email}
-            placeholder="Email"
-            onChangeText={(nextValue) => setEmail(nextValue)}
-          ></Input>
-          <Input
-            value={password1}
-            placeholder="Password"
-            onChangeText={(nextValue) => setPassword1(nextValue)}
-          ></Input>
-          <Input
-            value={password2}
-            placeholder="Confirm Password"
-            onChangeText={(nextValue) => setPassword2(nextValue)}
-          ></Input>
-          <Select
-           selectedIndex={selectedIndex}
-           value={ROLES[selectedIndex.row]}
-            onSelect={index => {
-            setSelectedIndex(index)
-          }}>
-            {ROLES.map(role => {
-              return <SelectItem key={role} title={role} value={role}/>
-            })}
-            </Select>
-            <Button onPress={initRegister}>Register</Button>
+      <View style={styles.container}>
+        <View style={styles.center}>
+         <View style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
+         <Image
+            style={styles.logo}
+            source={LogoIcon}
+          />
+          <Text
+            category="h2"
+            style={{ color: "#444",  ...styles.mg10 }}
+          >
+            Register
+          </Text>
+        
+         </View>
+        </View>
+    <View style={{marginTop:height*0.02}}>
+    <Input
+          value={username}
+          style={styles.mg10}
+          size="large"
+          placeholder="Username"
+          onChangeText={(nextValue) => setUsername(nextValue)}
+        ></Input>
+        <Input
+          value={email}
+          style={styles.mg10}
+          size="large"
+          placeholder="Email"
+          onChangeText={(nextValue) => setEmail(nextValue)}
+        ></Input>
+        <Input
+          value={password1}
+          placeholder="Password"
+          style={styles.mg10}
+          size="large"
+          type=""
+          onChangeText={(nextValue) => setPassword1(nextValue)}
+        ></Input>
+        <Input
+          value={password2}
+          placeholder="Confirm Password"
+          style={styles.mg10}
+          size="large"
+          onChangeText={(nextValue) => setPassword2(nextValue)}
+        ></Input>
+        <Select
+          selectedIndex={selectedIndex}
+          size="large"
+          value={ROLES[selectedIndex.row]}
+          onSelect={(index) => {
+            setSelectedIndex(index);
+          }}
+        >
+          {ROLES.map((role) => {
+            return <SelectItem key={role} title={role} value={role} />;
+          })}
+        </Select>
+    </View>
+        <Button
+          style={{
+            marginTop: 20,
+            backgroundColor: "#02BA76",
+            borderColor: "#02BA76",
+          }}
+          onPress={initRegister}
+        >
+          {registering ? "Loading..." : "Register"}
+        </Button>
 
-          <Text>
+        {/* <Text>
               Already have an account?
               <Text
                 style={{ color: "#222", fontWeight: "bold" }}
@@ -116,11 +149,52 @@ const RegisterScreen = ({navigation}) => {
                 {" "}
                 Login
               </Text>
-            </Text>
-        </>
-      )}
+            </Text> */}
+        <Text
+          style={{
+            marginTop: 20,
+            textAlign: "center",
+            fontWeight: "medium",
+            fontSize: 18,
+          }}
+        >
+          Already have an account?
+          <Text
+            style={{ color: "#02BA76", fontWeight: "bold", fontSize: 19 }}
+            onPress={() => {
+              navigation.goBack();
+            }}
+          >
+            {" "}
+            Login
+          </Text>
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default RegisterScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    height: "100%",
+    display: "flex",
+    paddingHorizontal: width * 0.03,
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  mg10: {
+    marginVertical: 7,
+  },
+  center: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: 80,
+    height: 80,
+  },
+});
