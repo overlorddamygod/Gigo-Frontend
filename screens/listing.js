c95d10223dd6223de07cc8b35c7d2dfa46ad963b
learningnoobi <fanime492@gmail.com>
2022-11-12 09:37:55 +0545

    feat : change home ui

--- screens/listing.js
+++ screens/listing.js
@@ -1,11 +1,14 @@
-import { Layout, Button, Text } from "@ui-kitten/components";
+import { Layout, Button, Text,Input } from "@ui-kitten/components";
 import { useState, useEffect} from "react"
+import {StyleSheet} from 'react-native'
 import Listing from "../components/listing";
 import SafeAreaView from "../components/SafeAreaView";
+import { width,height } from "../constant/size";
 import { axiosInstance } from "../utils/axios";
 
 const ListingScreen = ({ navigation }) => {
   const [listings, setListings] = useState([]);
+  const [searchInput, setSearchInput] = useState('')
 
   const getListings = async () => {
     try {
@@ -16,20 +19,35 @@ const ListingScreen = ({ navigation }) => {
     }
   };
 
+  const SearchCompany = async(e)=>{
+    try {
+      console.log(e)
+      setSearchInput(e)
+      const {data} = await axiosInstance.get(`/wsystem/company-list/?search=${e}`)
+      setListings(data);
+      
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   useEffect(() => {
     getListings();
-  
-    return () => {
-      
-    }
+
   }, [])
   
 
   return (
     // <SafeAreaView>
     <Layout style={{ flex: 1, padding: 10 }}>
-        {/* <Text category="h1">Listing</Text> */}
-        {listings.map(listing => {
+   <Input
+          style={styles.mg10}
+          value={searchInput}
+          placeholder="Search Company"
+          size="large"
+          onChangeText={SearchCompany}
+        ></Input>       
+         {listings.map(listing => {
           return <Listing key={listing.id} listing={listing} navigation={navigation} />
         })}
     </Layout>
@@ -38,3 +56,24 @@ const ListingScreen = ({ navigation }) => {
 };
 
 export default ListingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    height: "100%",
+    display: "flex",
+    paddingHorizontal: width * 0.03,
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  mg10: {
+    marginVertical: 10,
+  },
+  center: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection:'row'
+  },
+ 
+});
